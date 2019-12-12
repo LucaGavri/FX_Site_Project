92ed5e1077b3c0a87dab3d90d2e445614924b199
Lukasz Gawrys <lukaszgawrys13@gmail.com>
2019-12-12 21:22:41 +0100

    new

---
+++ docs/precache-manifest.9038e9d6ae155bb5aafb23819cacdc1f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c1f8c79c6f61a078e87b7890cd289b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0c4324c641ad8a45c9af",
+    "url": "./static/css/main.3542c0b9.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "./static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "0c4324c641ad8a45c9af",
+    "url": "./static/js/main.fc71fe80.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "be8c1a3941a46d6a90f55211e8d77239",
+    "url": "./static/media/8bit16-webfont.be8c1a39.woff2"
+  },
+  {
+    "revision": "dfc209a1ed53f74b62b539e7128ee4eb",
+    "url": "./static/media/8bit16-webfont.dfc209a1.woff"
+  },
+  {
+    "revision": "c0fb605aa119cfd32fea790218644c8f",
+    "url": "./static/media/backgroundwallstreet.c0fb605a.jpg"
+  },
+  {
+    "revision": "335961556cd64ad73d781845e0169333",
+    "url": "./static/media/fxlogo1.33596155.png"
+  },
+  {
+    "revision": "428b7928a914fe381f0e351107d9babb",
+    "url": "./static/media/photo3.428b7928.jpeg"
+  }
+]);
